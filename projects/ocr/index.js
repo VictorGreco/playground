657929e3c9f5ee1ee21c8e231a8e48650b9785fd
Victor Greco <33832977+VictorGreco@users.noreply.github.com>
2023-04-14 11:50:37 +0000

    fix: formated index.js ocr

--- projects/ocr/index.js
+++ projects/ocr/index.js
@@ -13,12 +13,12 @@ const { createWorker } = require('tesseract.js');
 
     const worker = await createWorker();
 
-  await worker.loadLanguage('eng');
-  await worker.initialize('eng');
-  await worker.setParameters({
-    tessedit_char_whitelist: '0123456789'
-  });
-  const { data: { text } } = await worker.recognize(temporalImageBlackAndWhite);
-  console.log(text);
-  await worker.terminate();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    await worker.setParameters({
+        tessedit_char_whitelist: '0123456789'
+    });
+    const { data: { text } } = await worker.recognize(temporalImageBlackAndWhite);
+    console.log(text);
+    await worker.terminate();
 })();
